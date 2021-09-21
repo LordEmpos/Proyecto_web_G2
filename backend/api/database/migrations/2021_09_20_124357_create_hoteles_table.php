dfaefa582c9c3cd2149d6e82109e84e1121ab31d
dklenneth <julian2704@live.com>
2021-09-21 06:46:49 -0500

    backend correcciones
    Correcciones a campos de las tablas

--- backend/api/database/migrations/2021_09_20_124357_create_hoteles_table.php
+++ backend/api/database/migrations/2021_09_20_124357_create_hoteles_table.php
@@ -4,25 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHotelsTable extends Migration
+class CreateHotelesTable extends Migration
 {
     
     public function up()
     {
-        Schema::create('hotels', function (Blueprint $table) {
+        Schema::create('hoteles', function (Blueprint $table) {
             $table->id();
             $table->string('nombre',50);
             $table->string('ciudad', 50);
             $table->tinyInteger('estrellas');
             $table->string('telefono', 14);
-
-            $table->primary('id');
         });
     }
 
     
     public function down()
     {
-        Schema::dropIfExists('hotels');
+        Schema::dropIfExists('hoteles');
     }
 }
