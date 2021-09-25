f57eae6225a8a3d4412838d7939e88bdfec24f41
dklenneth <julian2704@live.com>
2021-09-25 12:01:16 -0500

    Desarrollado en clase 25-09-2021
    Agregado aportes creados en clase 25-09-2021

--- backend/api/app/Models/Cliente.php
+++ backend/api/app/Models/Cliente.php
@@ -4,13 +4,34 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Cliente extends Model
-{
+class Cliente extends Authenticatable implements JWTSubject
+{   
+    use Notifiable;
+    public $timestamps = false;
+
     protected $table = 'clientes';
     protected $primaryKey = 'doc';
+    protected $fillable = ['doc', 'nombre', 'apellidos', 'correo', 'clave', 'registro'];
 
     public function reservas(){
         return $this->hasMany(reserva::class);
     }
+
+    //agregamos
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }   
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+    public function getAuthPassword(){
+        return $this->clave;
+    }
+
 }
--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\HabitacionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,21 @@ use App\Http\Controllers\ClienteController;
 |
 */
 
-Route::get('/Cliente', [ClienteController::class, 'showAll']);
+/* Route::get('/Cliente', [ClienteController::class, 'showAll']); */
+
+Route::post('/Cliente/signup', [ClienteController::class, 'signup']);
+Route::post('/Cliente/login', [ClienteController::class, 'login']);
+Route::get('/habitacion/search', [HabitacionController::class, 'search']);
+
+/* Route::get('/habitacion/filter', [HabitacionController::class, 'filter']); */
+/* Route::post('/cliente/update', [ClienteController::class, 'update']);  */
+
+Route::group(['middleware' => ['jwt.verify']], function(){
+        Route::get('/Cliente', [ClienteController::class, 'showAll']);
+        Route::post('/Cliente/update', [ClienteController::class, 'update']);
+        Route::post('/reservacion/create', [ReservaController::class, 'create']);
+        Route::post('/reservacion/history', [ReservaController::class, 'showByCliente']);
+    });
 
 /* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user(); 
